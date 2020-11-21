cd8b269b4fad38340b3c3851b324eee31a71a5fd
LeoChen <leograyc@gmail.com>
2020-11-21 00:13:49 -0500

    modified pandoc bat file to allow mutiple files

--- FileConvertor.bat
+++ FileConvertor.bat
@@ -1,1 +1,6 @@
-pandoc --extract-media ./mediaForDocs "Study Notes.docx" -t gfm -s -o output.md
+
+rmdir .\mediaForDocs /s /q
+
+pandoc --extract-media ./mediaForDocs/StudyNotesMid "Study Notes.docx" -t gfm -s -o StudyNotes.md
+
+@REM pandoc --extract-media ./mediaForDocs/test3Mid "test3.docx" -t gfm -s -o output2.md
---
+++ output2.md
@@ -0,0 +1,1 @@
+<img src="mediaForDocs\test3Mid\media\image1.png" style="width:2.69829in;height:2.0107in" />
